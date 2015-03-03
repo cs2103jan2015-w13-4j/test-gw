61eda7ea2b1a833f21925828b163dddb0dc79b5f
sonclark <sonclark1@gmail.com>
2015-03-03 21:33:16 +0800

    add ListTask

---
+++ src/ListTask.java
@@ -0,0 +1,39 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class ListTask {
+	
+	public static getList(){
+		
+	}
+	
+	
+	public static StringBuilder listTask(ArrayList<Task> taskList){
+		StringBuilder finalString = new StringBuilder("");
+		
+		for (Task t : taskList){
+			
+			String title = t.getTitle();
+			String startDate = dateFormatter(t.getStartDate());
+			String endDate = dateFormatter(t.getEndDate());
+			String startTime = t.getStartTime();
+			String endTime = t.getEndTime();
+			
+			String tmp = title+"/r/nStart :"+startDate+" @ "+startTime+"/r/nEnd :"+endDate+" @ "+endTime+"/r/n";
+			finalString.append(tmp);
+		}
+		
+		return finalString;
+	}
+	
+	private String dateFormatter(long ms){
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy");
+		Calendar calendar  = Calendar.getInstance();
+		calendar.setTimeInMillis(ms);
+
+		return sdf.format(calendar.getTime());
+	}
+	
+	
+}
